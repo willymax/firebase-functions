a0b1a1fd342cdd696a7cbe08fe2f4fc8998954a1
william makau <williammakau070@gmail.com>
2020-03-01 01:21:56 +0300

    Getting and Commenting on Scream

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -20,18 +20,75 @@ exports.getAllScreams = (req, res) => {
 };
 
 exports.postOneScream = (req, res) => {
-    const newScream = {
-      body: req.body.body,
-      userHandle: req.user.handle,
-      createdAt: new Date().toISOString()
-    };
-    db.collection("screams")
-      .add(newScream)
-      .then(doc => {
-        return res.json({ message: `document ${doc.id} created successfully` });
-      })
-      .catch(err => {
-        console.log(err);
-        return res.status(500).json({ error: "something went wrong" });
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.user.handle,
+    createdAt: new Date().toISOString()
+  };
+  db.collection("screams")
+    .add(newScream)
+    .then(doc => {
+      return res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      console.log(err);
+      return res.status(500).json({ error: "something went wrong" });
+    });
+};
+
+// Get one scream
+exports.getScream = (req, res) => {
+  let screamData = {};
+  db.doc(`/screams/${req.params.screamId}`)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(400).json({ error: "Scream not found" });
+      }
+      screamData = doc.data();
+      screamData.screamId = doc.id;
+      return db
+        .collection("comments")
+        .orderBy('createdAt', 'desc')
+        .where("screamId", "==", req.params.screamId)
+        .get();
+    })
+    .then(data => {
+      screamData.comments = [];
+      data.forEach(doc => {
+        screamData.comments.push(doc.data());
       });
+      return res.json(screamData);
+    })
+    .catch(err => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
+// Comment on a scream
+exports.commentOnScream = (req, res) => {
+  if(req.body.body.trim() === '') return res.status(400).json({error: 'Must not be empty'})
+  const newComment = {
+    body: req.body.body,
+    createdAt: new Date().toISOString(),
+    screamId: req.params.screamId,
+    userHandle: req.user.handle,
+    userImage: req.user.imageUrl
   }
+
+  db.doc(`/screams/${req.params.screamId}`).get()
+  .then(doc => {
+    if(!doc.exists){
+      return res.status(404).json({error: 'Scream not found'})
+    }
+    return db.collection('comments').add(newComment)
+  })
+  .then(() => {
+    return res.json(newComment)
+  })
+  .catch(err => {
+    console.error(err)
+    return res.status(500).json({error: 'Something went wrong'})
+  })
+}
--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,29 @@
-const functions = require('firebase-functions');
-const app = require('express')();
-const { getAllScreams, postOneScream } = require('./handlers/screams');
-const { signUp, login, uploadImage, addUserDetails, getAuthenticatedUser } = require('./handlers/users');
-const { FBAuth } = require('./utils/FBAuth');
+const functions = require("firebase-functions");
+const app = require("express")();
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream
+} = require("./handlers/screams");
+const {
+  signUp,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser
+} = require("./handlers/users");
+const { FBAuth } = require("./utils/FBAuth");
 
 // scream routes
 app.get("/screams", getAllScreams);
-
 app.post("/screams", FBAuth, postOneScream);
+app.get("/screams/:screamId", getScream);
+
+// TODO: delete scream
+// TODO: like a scream
+// TODO: unlike a scream
+app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
 
 // users routes
 app.post("/signup", signUp);
